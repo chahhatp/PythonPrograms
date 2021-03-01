32969c07d51ec823db0b9f64b9f9fe320d0cc15a
MrHackIT <rakesh127001@gmail.com>
2021-03-01 23:09:34 +0530

    Handling Command-line Arguments

--- mac_changer.py
+++ mac_changer.py
@@ -1,4 +1,11 @@
 import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+
+parser.parse_args()
 
 interface = input("interface > ")
 new_mac = input("new MAC >")
@@ -7,14 +14,4 @@ print("[+] Changing MAC address for " + interface + " to " + new_mac)
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
 subprocess.call(["ifconfig", interface, "up"])
-
-
-
-
-
-
-
-
-
-
 
