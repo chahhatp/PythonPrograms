b8b2317535264aa2c5c67c5fe0d18fc502c432d6
MrHackIT <rakesh127001@gmail.com>
2021-03-01 22:49:53 +0530

    Variables_Strings

--- mac_changer.py
+++ mac_changer.py
@@ -1,4 +1,8 @@
 import subprocess
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+
+interface = "wlan0"
+new_mac = "00:11:22:33:44:77"
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+# subprocess.call("ifconfig wlan0 down", shell=True)
+# subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
+# subprocess.call("ifconfig wlan0 up", shell=True)
