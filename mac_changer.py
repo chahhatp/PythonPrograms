16c979e33353a2bfa169350d3d02e1c4d0df9640
MrHackIT <rakesh127001@gmail.com>
2021-03-01 22:53:41 +0530

    Using Variables in MAC Changer

--- mac_changer.py
+++ mac_changer.py
@@ -1,8 +1,8 @@
 import subprocess
 
-interface = "wlan0"
-new_mac = "00:11:22:33:44:77"
+interface = "eth0"
+new_mac = "00:11:22:33:44:88"
 print("[+] Changing MAC address for " + interface + " to " + new_mac)
-# subprocess.call("ifconfig wlan0 down", shell=True)
-# subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66", shell=True)
-# subprocess.call("ifconfig wlan0 up", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
