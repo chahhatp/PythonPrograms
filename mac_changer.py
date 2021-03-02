8ce9ada2e9079921322c6962238e7e0d9fd2165a
chahhat <chahhatp@gmail.com>
2021-03-02 17:04:48 -0500

    mac changer with validations

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 import subprocess
 import optparse
-
+import re
+import string
 # interface =  input("Interface -> ")
 # new_mac = input("New MAC -> ")
 
@@ -33,7 +34,7 @@ def change_mac(interface, new_mac):
 
 options = get_args()
 
-change_mac(options.interface, options.new_mac)
+#change_mac(options.interface, options.new_mac)
 
 
 
@@ -44,6 +45,34 @@ change_mac(options.interface, options.new_mac)
 # subprocess.call(["sudo", "ifconfig", interface, "down"])
 # subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
 # subprocess.call(["sudo", "ifconfig", interface, "up"])
+
+def return_mac_address(interface):
+    return_value_of_ifconfig = str(subprocess.check_output(["ifconfig", interface]))
+
+    mac_return = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", return_value_of_ifconfig)
+    if mac_return:
+        return mac_return.group(0)
+    else:
+        print("Cannot get the mac address")
+
+
+
+current_mac = return_mac_address(options.interface)
+
+print("Current Mac is "+ str(current_mac))
+
+change_mac(options.interface, options.new_mac)
+current_mac2 = return_mac_address(options.interface)
+
+
+if current_mac2.lower() == options.new_mac.lower():
+    print("Mac changed to " + current_mac2)
+else:
+    print("Mac Changed")
+
+
+
+
 
 
 
